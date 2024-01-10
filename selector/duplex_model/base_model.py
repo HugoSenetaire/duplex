503a748eab4fc091f2bd3d0238b3da01abb04676
Hugo Senetaire <hhjs@dtu.dk>
2024-01-10 15:15:43 +0100

    fix : save net not module

--- selector/duplex_model/base_model.py
+++ selector/duplex_model/base_model.py
@@ -167,7 +167,7 @@ class BaseModel(ABC):
 
                 if name != "AUX":
                     if len(self.gpu_ids) > 0 and torch.cuda.is_available():
-                        torch.save(net.module.cpu().state_dict(), save_path)
+                        torch.save(net.cpu().state_dict(), save_path)
                         net.cuda(self.gpu_ids[0])
                     else:
                         torch.save(net.cpu().state_dict(), save_path)

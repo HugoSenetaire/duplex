367e87b3d908d7be2b2a0411654ae3fbe2860ad7
Hugo Senetaire <hhjs@dtu.dk>
2024-01-10 22:18:21 +0100

    fix : typo x_tilde_no_temp

--- selector/duplex_model/pathwise_selector_model.py
+++ selector/duplex_model/pathwise_selector_model.py
@@ -152,7 +152,7 @@ class PathWiseSelectorModel(BaseModel):
         
         # Create mixed images
         self.x_tilde = (self.x_expanded * self.z + (1 - self.z) * self.x_cf_expanded).flatten(0,1)
-        self.x_tilde_test = (self.x_expanded * self.z_notemp + (1 - self.z_notemp) * self.x_cf_expanded).flatten(0,1)
+        self.x_tilde_notemp = (self.x_expanded * self.z_notemp + (1 - self.z_notemp) * self.x_cf_expanded).flatten(0,1)
 
 
         self.z_to_save = (self.z.flatten(0,1) * 2) -1

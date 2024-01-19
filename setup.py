91da12f409d6f80920e62e78555934de3bcf8cfa
Diane Adjavon <adjavond@hhmi.org>
2024-01-19 09:45:41 -0500

    chore: Remove dapi from install
    Only installs selector, since DAPI will be in a separate package

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='dapi',
+    name='duplex',
     version='0.1',
     description='dapi',
     url='https://github.com/anon/dapi',
     author='anon',
     packages=[
-        'dapi',
-        'dapi_networks',
         'selector'
         ])

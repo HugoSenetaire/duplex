b2fad09b6fa29f005eea15ab4adc73b3578bfc6e
HugoSenetaire <hhjs@dtu.dk>
2024-02-24 11:29:45 -0500

    feat : use constant lambda by default

--- selector/duplex_trainer/pathwise_trainer.py
+++ selector/duplex_trainer/pathwise_trainer.py
@@ -34,7 +34,7 @@ class PathWiseTrainer(BaseTrainer):
 
         parser.add_argument('--lambda_regularization', type=float, default=1.0, help='L1 regularization strenght to limit the selection of the mask')
         parser.add_argument('--lambda_regularization_init', type=float, default=0., help='Initial value for the lambda_regularization scheduler')
-        parser.add_argument('--lambda_regularization_scheduler', type=str, default='linear', help='Scheduler for the lambda_regularization parameter. [linear | constant | cosine]')
+        parser.add_argument('--lambda_regularization_scheduler', type=str, default='constant', help='Scheduler for the lambda_regularization parameter. [linear | constant | cosine]')
         parser.add_argument('--lambda_regularization_scheduler_targetepoch', type=int, default=10, help='Target epoch for the lambda_regularization scheduler to reach the lambda_regularization value')
 
         parser.add_argument('--lambda_ising_regularization', type=float, default=-1.0, help='Ising regularization strenght to enforce connectivity in the mask selection')

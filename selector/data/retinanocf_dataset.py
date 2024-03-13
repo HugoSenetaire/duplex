0f8c509b64031139ad927c5f2a03d6d2668334a1
HugoSenetaire <hhjs@dtu.dk>
2024-03-13 10:57:33 -0400

    feat : add correct transform to retina dataset

--- selector/data/retinanocf_dataset.py
+++ selector/data/retinanocf_dataset.py
@@ -7,6 +7,9 @@ import torch
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import os
+import albumentations as A
+import albumentations.pytorch as apytorch
+from PIL import Image
 
 
 logging.basicConfig(level=logging.WARNING)
@@ -38,6 +41,15 @@ class RetinaNoCFDataset(BaseDataset,):
     A dataset for which a dummy counterfactual and a dummy target is returned along the true image.
     It assumes the images can be loaded with the ImageFolder dataset.
     """
+
+    def __scale(self, x, target, method=Image.BICUBIC):
+        ow, oh = x.size
+        if (ow == target):
+            return x
+        w = target
+        h = target
+        return x.resize((w, h), method)
+
     def __init__(
         self,
         opt,
@@ -56,22 +68,23 @@ class RetinaNoCFDataset(BaseDataset,):
 
         # Dataset
 
+        # Dataset
+
         if self.augment and self.split == "train":
-            self.transform = transforms.Compose(
+            self.transform = A.Compose(
                 [
-                    transforms.RandomHorizontalFlip(p=0.5),
-                    transforms.RandomVerticalFlip(p=0.5),
-                    transforms.RandomRotation(90),
-                    transforms.Normalize(mean=(0.485,0.456,0.406), std=(0.229, 0.224, 0.225)),
-                    transforms.ToTensor(),
+                    A.HorizontalFlip(p=0.5),
+                    A.VerticalFlip(p=0.5),
+                    A.RandomRotate90(p=0.5),
+                    apytorch.transforms.ToTensorV2(),
+
                 ],
 
             )
         else:
-            self.transform = transforms.Compose(
+            self.transform = A.Compose(
             [
-                    transforms.Normalize(mean=(0.485,0.456,0.406), std=(0.229, 0.224, 0.225)),
-                    transforms.ToTensor(),
+                apytorch.transforms.ToTensorV2()
             ],
         )
 
@@ -79,29 +92,30 @@ class RetinaNoCFDataset(BaseDataset,):
             root=root,
             transform=None,
         )
-        print(np.array(self.dataset.__getitem__(0)[0]).shape)
+
+        self.idx_to_class = {k: k for k in self.dataset.classes}
 
 
-    def __make_dict(self, x, xcf, y, ycf, x_path):
+    def __make_dict(self, x, xcf, y, ycf, x_path = "None",):
         return {
             "x": x,
             "x_cf": xcf,
             "y": y,
             "y_cf": ycf,
             "x_path": x_path,
-            "xcf_path": "none",
+            "x_cf_path": "None",
         }
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, index):
-        self.dataset.__getitem__(index)
         x, y, path = self.dataset.__getitem__(index)
-        # x = np.array(x, dtype=np.float32)[..., 0] # It's a grayscale image, so we only need one channel
-        # x = self.transform(image=x)["image"]/255 *2 -1 # Normalize to [-1, 1]
+        x = self.__scale(x, self.opt.load_size)
+        x = np.array(x, dtype=np.float32) # It's a grayscale image, so we only need one channel
+        x = self.transform(image=x)["image"]/255 *2 -1 # Normalize to [-1, 1]
         y = torch.tensor(y, dtype=torch.long)
         # target = np.random.choice([i for i in range(1, 6) if i != y])
         target = torch.full_like(y, -1)
         x_cf = torch.full_like(x, -1)
-        return self.__make_dict(x, x_cf, y, target, path)
+        return self.__make_dict(x, x_cf, y, target, x_path=path)
